88bc3e7bae1a1f74b688483cb7ec71d0c340cd4b
Jan Martinek <xmarti19@fi.muni.cz>
2022-10-14 19:09:13 +0200

    added flatlaf theme as default

--- src/main/java/cz/muni/fi/pv168/Main.java
+++ src/main/java/cz/muni/fi/pv168/Main.java
@@ -1,18 +1,44 @@
 package cz.muni.fi.pv168;
 
 import cz.muni.fi.pv168.gui.frames.MainWindow;
+import com.formdev.flatlaf.*;
 
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        initNimbusLookAndFeel();
+        initFlatlafLookAndFeel("intellij"); // can be reworked to load preconfigured
         SwingUtilities.invokeLater(MainWindow::new);
+    }
+
+    /**
+     * Call before creating main window. Sets the look and feel to Flatlaf:
+     * <pre>
+     * Possible options:
+     * intellij = light theme modeled after IntelliJ
+     * darcula = dark theme modeled after Darcula
+     * dark = default flatlaf dark theme
+     * light = default flatlaf light theme
+     * </pre>
+     * If none of those is used as an argument, sets the theme to NIMBUS.
+     */
+    private static void initFlatlafLookAndFeel(String theme) {
+        if (theme != null && theme.toLowerCase().equals("intellij")) {
+            FlatIntelliJLaf.setup();
+        } else if (theme != null && theme.toLowerCase().equals("darcula")) {
+            FlatDarculaLaf.setup();
+        } else if (theme != null && theme.toLowerCase().equals("dark")) {
+            FlatDarkLaf.setup();
+        } else if (theme != null && theme.toLowerCase().equals("light")) {
+            FlatLightLaf.setup();
+        } else {
+            initNimbusLookAndFeel();
+        }
     }
 
     private static void initNimbusLookAndFeel() {
