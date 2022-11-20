4e5e248a1d47db100c3a3d7b21c60705e2f6309f
Adam Slíva <xsliva@fi.muni.cz>
2022-11-20 16:53:02 +0100

    Added DatabaseManager test

---
+++ src/test/java/cz/muni/fi/pv168/data/storage/db/DatabaseManagerTest.java
@@ -0,0 +1,35 @@
+package cz.muni.fi.pv168.data.storage.db;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class DatabaseManagerTest {
+    private DatabaseManager manager;
+
+    @BeforeEach
+    void setUp() {
+        this.manager = DatabaseManager.createTestInstance();
+    }
+
+    @AfterEach
+    void tearDown() {
+        manager.destroySchema();
+    }
+
+    /**
+     * The {@link DatabaseManager#initSchema()} is called inside {@link DatabaseManager#createTestInstance()}
+     * this is the second time it is called
+     */
+    @Test
+    void schemaInitializationShouldBeIdempotent() {
+        assertThatCode(() -> manager.initSchema()).doesNotThrowAnyException();
+    }
+
+    @Test
+    void destroySchemaShouldByValid() {
+        assertThatCode(() -> manager.destroySchema()).doesNotThrowAnyException();
+    }
+}
