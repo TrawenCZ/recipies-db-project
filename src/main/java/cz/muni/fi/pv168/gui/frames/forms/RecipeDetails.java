9b8e5ed2e22de2dbcdde67be980329a9c97e321d
Jan Martinek <xmarti19@fi.muni.cz>
2022-11-29 14:56:24 +0100

    updates/singleIngredient-forms

--- src/main/java/cz/muni/fi/pv168/gui/frames/forms/RecipeDetails.java
+++ src/main/java/cz/muni/fi/pv168/gui/frames/forms/RecipeDetails.java
@@ -1,80 +1,19 @@
 package cz.muni.fi.pv168.gui.frames.forms;
 
-import javax.swing.*;
-
-import cz.muni.fi.pv168.gui.elements.ScrollPane;
+import cz.muni.fi.pv168.gui.elements.SingleIngredient;
 import cz.muni.fi.pv168.model.*;
 
-import java.awt.*;
+import java.util.List;
 
 public class RecipeDetails extends RecipeForm {
 
-    private final ScrollPane<SingleIngredient> ingredients = new ScrollPane<>();
-
-    private class SingleIngredient extends JPanel {
-
-        private JTextField ingredient = new JTextField(16);
-        private JTextField unit = new JTextField(16);
-        private JTextField amount = new JTextField(6);
-
-        public SingleIngredient(RecipeIngredient ingredientAmount) {
-            super(new GridBagLayout());
-
-            if (ingredientAmount == null) throw new NullPointerException();
-
-            this.ingredient.setText(ingredientAmount.getIngredient().getName());
-            this.amount.setText(String.format("%.2f", ingredientAmount.getAmount()));
-            this.unit.setText(ingredientAmount.getUnit().getName());
-
-            this.ingredient.setEditable(false);
-            this.amount.setEditable(false);
-            this.unit.setEditable(false);
-
-            this.ingredient.setHorizontalAlignment(SwingConstants.CENTER);
-            this.amount.setHorizontalAlignment(SwingConstants.RIGHT);
-            this.unit.setHorizontalAlignment(SwingConstants.CENTER);
-
-            initializeLayout();
-            ingredients.addTyped(this);
-        }
-
-        private void initializeLayout() {
-            GridBagConstraints c = new GridBagConstraints();
-            c.fill = GridBagConstraints.HORIZONTAL;
-            c.ipady = 10;
-
-            c.weightx = 2;
-            c.gridx = 0;
-            this.add(ingredient, c);
-
-            c.weightx = 0.5;
-            c.gridx = 1;
-            this.add(amount, c);
-
-            c.weightx = 1;
-            c.gridx = 2;
-            this.add(unit, c);
-        }
-    }
-
     public RecipeDetails(Recipe recipe) {
-        super(recipe.getName());
-        initializeBody();
-        pack();
-        setData(recipe);
-        setIngredients(recipe);
-        show();
+        super(recipe, true);
     }
 
     @Override
     protected void initializeBody() {
         super.initializeBody();
-
-        // replace original ingredients with locked ones
-        body.remove(super.ingredients);
-
-        ingredients.setPreferredSize(new Dimension(410, 500));
-        gridAdd(ingredients, 4, 1, 2, 10);
 
         // set all to non-editable and hide the ingredients add button
         nameInput.setEditable(false);
@@ -91,7 +30,10 @@ public class RecipeDetails extends RecipeForm {
         return true;
     }
 
-    private void setIngredients(Recipe recipe) {
-        super.setIngredients(recipe, SingleIngredient::new);
+    @Override
+    protected void setIngredients(List<RecipeIngredient> recipeIngredients) {
+        for (var i : recipeIngredients) {
+            ingredients.addTyped(new SingleIngredient(recipe, i));
+        }
     }
 }
